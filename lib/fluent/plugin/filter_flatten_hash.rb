b1b9f8dd4932f472051e2f2c846210ef1bbebe14
Erwan Miran <mirwan666@gmail.com>
2017-01-06 09:24:53 +0100

    ability to flatten one field

--- lib/fluent/plugin/filter_flatten_hash.rb
+++ lib/fluent/plugin/filter_flatten_hash.rb
@@ -7,13 +7,38 @@ module Fluent
 
     config_param :separator, :string, :default => '.'
     config_param :flatten_array, :bool, :default => true
+    config_param :only_key, :string, :default => nil
     
     def configure(conf)
-      super
+      if !@only_key.nil?
+        begin
+          @only_key = @only_key.split(".")
+        rescue => e
+          raise Fluent::ConfigError, "only_key is illegal! only_key=#{@only_key}"
+        end
+      end
     end
 
     def filter(tag, time, record)
-      flatten_record(record, [])
+        if !@only_key.nil?
+          item = record
+          path = @only_key
+          prefix = path.pop
+
+          path.delete_if do |k|
+            if item.has_key?(k)
+              item = item[k]
+              true
+            end
+          end
+          if !path.empty? or !item.has_key?(prefix)
+            record
+          else
+	    flatten_record(item[prefix], [prefix])
+          end
+        else
+          flatten_record(record, [])
+        end
     end
   end if defined?(Filter)
 end
